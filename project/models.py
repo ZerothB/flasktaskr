333e209387bec9e492538d1458432fe143fedb11
Giovanni <giovannibarbarani@gmail.com>
2015-10-03 12:47:42 +0200

    db_create, is an obsolete scripts, maybe need only to create db

--- project/models.py
+++ project/models.py
@@ -1,19 +1,45 @@
 from views import db
+import datetime
 
 class Task(db.Model):
 
 	__tablename__="tasks"
 
 	task_id = db.Column(db.Integer, primary_key=True)
+	user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 	name = db.Column(db.String, nullable=False)
 	due_date = db.Column(db.Date, nullable=False)
+	priority = db.Column(db.Integer, nullable=False)
+	posted_date = db.Column(db.Date, default=datetime.datetime.utcnow())
 	status = db.Column(db.Integer)
 
-	def __init__(self, name, due_date, priority, status):
+
+	def __init__(self, name, due_date, priority, posted_date, status, user_id):
 		self.name = name
 		self.due_date = due_date
 		self.priority = priority
 		self.status = status
+		self.user_id = user_id
+		self.posted_date = posted_date
 
 	def __repr__(self):
 		return '<name {}'.format(self.name)
+
+class User(db.Model):
+	"""docstring for User"""
+
+	__tablename__ = 'users'
+
+	id = db.Column(db.Integer, primary_key=True)
+	tasks = db.relationship('Task', backref='poster')
+	name = db.Column(db.String, unique=True, nullable=False)
+	email = db.Column(db.String, unique=True, nullable=True)
+	password = db.Column(db.String, nullable=False)
+
+	def __init__(self, name=None, email=None, password=None):
+		self.name = name
+		self.email = email
+		self.password = password
+
+	def __repr__(self):
+		return '<User {}>'.format(self.name)
